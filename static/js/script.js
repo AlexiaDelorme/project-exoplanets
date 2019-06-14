1b0323482f1d9f07a69ee6f1aa011577869f32e5
Alexia Delorme <alexia.delorme@gmail.com>
2019-06-14 13:51:46 +0000

    Added reset filter button in modal form and added code to reset modal checkboxes to checked when button is clicked

--- static/js/script.js
+++ static/js/script.js
@@ -1,84 +1,101 @@
-//Helper function to reset button to "checked"
-function resetButtonChecked(element) {
-    if ($(element).checked != true) {
-        $(element).click();
-    }
-}
+$(document).ready(function() {
 
-//Helper function to reset entire modal form
-function resetModalForm() {
-    resetButtonChecked("#switchTable");
-    resetButtonChecked("#switchDataType");
-    resetButtonChecked("#switchCorr1");
-    resetButtonChecked("#switchCorr2");
-    resetButtonChecked("#switchCorr3");
-}
+    // Helper function to reset checkbox button to "checked"
+    function resetButtonChecked(element) {
+        if (!$(element).is(':checked')) {
+            $(element).click();
+        }
+    }
 
-//Clear all filters to chart to start over
-$('#reset-filters').click(function() {
+    // Clear all filters on dashboard and modal form to start over
+    $('.reset-filters').click(function() {
 
-    dc.filterAll();
-    dc.renderAll();
+        dc.filterAll();
+        dc.renderAll();
 
-    //reset modal form to default settings
-    resetButtonChecked("#switchTable");
-});
+        // Reset modal form to default settings $("input:checkbox")
+        resetButtonChecked("#switchTable");
+        resetButtonChecked("#switchDataType");
+        resetButtonChecked("#switchCorr1");
+        resetButtonChecked("#switchCorr2");
+        resetButtonChecked("#switchCorr3");
 
-// Automatically redirect user to "discovery" section when clicking modal button
-$('#button-modal').click(function() {
-    $('#myModal').modal('hide');
-    $(document).scrollTop($('#discovery').offset().top);
-});
+    });
 
-$("#switchTable").change(function() {
-    if (this.checked != true) {
-        $("#recap-data").hide();
-        $("#nav-item-table").hide();
-        $("#checkboxLabelTable").text("Disable");
-    }
-    else {
-        $("#recap-data").show();
-        $("#nav-item-table").show();
-        $("#checkboxLabelTable").text("Enable");
-    }
-});
+    // Automatically redirect user to "discovery" section when clicking modal button
+    $('#button-modal').click(function() {
+        $('#myModal').modal('hide');
+        $(document).scrollTop($('#discovery').offset().top);
+    });
 
-$("#switchDataType").change(function() {
-    if (this.checked != true) {
-        $("#non-cumulative-chart").hide();
-        $("#cumulative-chart").show();
-        $("#checkboxLabelData").text("Cumulative");
-    }
-    else {
-        $("#non-cumulative-chart").show();
-        $("#cumulative-chart").hide();
-        $("#checkboxLabelData").text("Non Cumulative");
-    }
-});
+    // Display/hide "Data Table" section
+    $("#switchTable").change(function() {
+        if (this.checked != true) {
+            $("#recap-data").hide();
+            $("#nav-item-table").hide();
+            $("#checkboxLabelTable").text("Disable");
+        }
+        else {
+            $("#recap-data").show();
+            $("#nav-item-table").show();
+            $("#checkboxLabelTable").text("Enable");
+        }
+    });
 
-$("#switchCorr1").change(function() {
-    if (this.checked != true) {
-        $("#planet-corr-section").hide();
-    }
-    else {
-        $("#planet-corr-section").show();
-    }
-});
+    // Display cumulative or non-cumulative data type for years of discovery bar chart
+    $("#switchDataType").change(function() {
+        if (this.checked != true) {
+            $("#non-cumulative-chart").hide();
+            $("#cumulative-chart").show();
+            $("#checkboxLabelData").text("Cumulative");
+        }
+        else {
+            $("#non-cumulative-chart").show();
+            $("#cumulative-chart").hide();
+            $("#checkboxLabelData").text("Non Cumulative");
+        }
+    });
 
-$("#switchCorr2").change(function() {
-    if (this.checked != true) {
-        $("#mass-corr-section").hide();
-    }
-    else {
-        $("#mass-corr-section").show();
-    }
-});
+    // Hide/show planet mass vs. planet radius correlation graph
+    $("#switchCorr1").change(function() {
+        if (this.checked != true) {
+            $("#planet-corr-section").hide();
+        }
+        else {
+            $("#planet-corr-section").show();
+        }
+    });
 
-$("#switchCorr3").change(function() {
-    if (this.checked != true) {
-        $("#radius-corr-section").hide();
-    }
-    else {
-        $("#radius-corr-section").show();
-    }
+    // Hide/show planet mass vs. stellar mass correlation graph
+    $("#switchCorr2").change(function() {
+        if (this.checked != true) {
+            $("#mass-corr-section").hide();
+        }
+        else {
+            $("#mass-corr-section").show();
+        }
+    });
+
+    // Hide/show planet radius vs. stellar radius correlation graph
+    $("#switchCorr3").change(function() {
+        if (this.checked != true) {
+            $("#radius-corr-section").hide();
+        }
+        else {
+            $("#radius-corr-section").show();
+        }
+    });
+
+    //Hide Correlation menu item in navbar when the three checkboxes for correlation graphs have been unchecked
+    $(".correlation-checkbox").change(function() {
+        if (!$(".correlation-checkbox").is(':checked')) {
+            $("#nav-item-correlation").hide();
+            $("#correlation").hide();
+        }
+        else {
+            $("#nav-item-correlation").show();
+            $("#correlation").show();
+        }
+    });
+
 });
